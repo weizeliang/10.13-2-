74c4ac1df200b68e70b8162cff9c798cf80f61e1
weizeliang <2417265258@qq.com>
2019-10-13 20:17:27 +0800

    第一次提交

---
+++ src/main/java/test/Hello.java
@@ -0,0 +1,15 @@
+package test;
+/**
+ * 
+    * @ClassName: Hello
+    * @Description: Holle类
+    * @author DELL
+    * @date 2019年9月28日
+    *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("1111111");
+	}
+}
---
+++ src/test/java/test/TestHello.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * 
+    * @ClassName: TestHello
+    * @Description:TestHello测试类
+    * @author DELL
+    * @date 2019年9月28日
+    *
+ */
+public class TestHello {
+
+	public static void main(String[] args) {
+		System.out.println("22222222");
+	}
+}
